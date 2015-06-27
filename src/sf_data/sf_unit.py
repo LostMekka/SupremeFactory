3b2bddcdc356b21ac8c5f798e9c0200f4d70cc14
Gilbert Röhrbein <gilbert@ifsr.de>
2015-06-27 17:28:56 +0200

    moving 'elefants'

--- src/sf_data/sf_unit.py
+++ src/sf_data/sf_unit.py
@@ -1,7 +1,69 @@
-class Unit:
+from utils import *
+import random as R
+import pygame
+from pygame.sprite import *
 
-    pos = 0
-    speed = 0
-    hp = 10
-    attack = 1
-    range = 0
+
+
+def unit_stuff():
+    anim_paths  = ["../assets/friend"+str(i)+".png" for i in range(1,4)]
+    anim_surfs  = [pygame.image.load(path) for path in anim_paths]
+    group       = Group()
+    for i in range(10):
+        sprite          = BfUnit(
+            anim    = Anim(anim_surfs),
+            move    = UnitMove((8, 12)))
+        group.add(sprite)
+    return group
+
+
+
+class Anim:
+
+    def __init__(self, surfs):
+        self.surfs  = surfs
+        self.index  = 0
+        self.delay  = 0.9
+        self.__time = 0
+
+    def update(self, dt):
+        self.__time = self.__time + dt
+        while self.__time > self.delay:
+            self.index = (self.index + 1) % len(self.surfs)
+            self.__time = self.__time - self.delay
+
+    def image(self):
+        return self.surfs[self.index % len(self.surfs)]
+
+
+
+class UnitMove:
+
+    def __init__(self, speed):
+        import random
+        if isinstance(speed, tuple):
+            self.speed = random.uniform(*speed)
+        else:
+            self.speed = speed
+        self.pos = 0
+
+    def update(self, dt):
+        self.pos = self.pos + self.speed * dt
+
+
+
+class BfUnit(Sprite):
+
+    def __init__(self, anim, move):
+        super(BfUnit, self).__init__()
+        self.anim   = anim
+        self.move   = move
+        self.image  = self.anim.image()
+        self.rect   = self.image.get_rect()
+
+    def update(self, dt):
+        self.anim.update(dt)
+        self.move.update(dt)
+        self.image  = self.anim.image()
+        self.rect.x = self.move.pos
+
