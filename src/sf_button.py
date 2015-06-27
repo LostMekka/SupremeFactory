5fece9522a0d488c9d94f6dd5a624c38906e5995
LostMekka <lostmekka@web.de>
2015-06-28 00:10:46 +0200

    added callback object and state modifier methods to button

--- src/sf_button.py
+++ src/sf_button.py
@@ -2,39 +2,52 @@ import pygame
 from utils import *
 
 class SFButton:
-    def __init__(self, rect, color, text, text_color, callback):
+    def __init__(self, rect, color, text, text_color, callback, callback_object):
         self.rect = rect
         self.color = color
         self.text = text
         self.text_color = text_color
         self.callback = callback
+        self.callback_object = callback_object
+        self.active = True
 
-    def write_text(self, surface, font_size=14):
+    def set_available(self):
+        self.color = (0, 50, 0)
+        self.text_color = (0, 255, 0)
+        self.active = True
+
+    def set_unavailable(self):
+        self.color = (50, 0, 0)
+        self.text_color = (255, 0, 0)
+        self.active = False
+
+    def set_deactivated(self):
+        self.color = (0, 50, 0)
+        self.text_color = (150, 150, 150)
+        self.active = False
+
+    def draw(self, surface, font_size=14):
         """
-        Writes the buttons text to the given surface (like draw(), only for the text).
+        Draws the button to the given surface.
         Font size defaults to 14.
         :param surface:
         :param font_size:
         :return:
         """
+        pygame.draw.rect(surface, self.color, self.rect, 0)
+        pygame.draw.rect(surface, (190, 190, 190), self.rect, 1)
         font = pygame.font.SysFont(None, font_size)
         this_text = font.render(self.text, 1, self.text_color)
         surface.blit(this_text, ((self.rect[0] + self.rect[2] / 2) - this_text.get_width() / 2,
                                  (self.rect[1] + self.rect[3] / 2) - this_text.get_height() / 2))
         return surface
-
-    def draw(self, surface):
-        """
-        Draws the button to the given surface.
-        :param surface:
-        :return:
-        """
-        pygame.draw.rect(surface, self.color, self.rect, 0)
-        pygame.draw.rect(surface, (190, 190, 190), self.rect, 1)
-        return surface
     
     def press(self):
-        self.callback(self)
+        if self.active:
+            if self.callback_object:
+                self.callback(self.callback_object)
+            else:
+                self.callback(self)
     
     def is_inside(self, point):
         return is_point_in_rect(mouse, self.rect)
@@ -46,7 +59,8 @@ class SFButton:
         :return:
         """
         if is_point_in_rect(mouse, self.rect):
-            self.press()
+            if self.active:
+                self.press()
             return True
         return False
 
