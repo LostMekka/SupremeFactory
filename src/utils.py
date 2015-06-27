86f345e467222ef9e254d261d6b3acd2fc7c513a
Josephine Rehak <justallphine@googlemail.com>
2015-06-27 14:11:00 +0200

    added colors

--- src/utils.py
+++ src/utils.py
@@ -19,5 +19,9 @@ class Duct(dict):
 Colors = Duct(
     white=(255, 255, 255),
     light_gray=(200, 200, 200),
-    gray=(150, 150, 150)
+    gray=(150, 150, 150),
+	black = (0, 0, 0),
+	red = (255, 0, 0),
+	green = (0, 255, 0),
+	blue = (0, 0, 255)
 )
