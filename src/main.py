5e2b947c03e9822fa2cd5c7cb977f30d1f1ce738
Kilian Koeltzsch <me@kilian.io>
2015-06-27 11:55:46 +0200

    Increase game res

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+DISPLAYSURF = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption('Hello World!')
 while True:  # main game loop
     for event in pygame.event.get():
