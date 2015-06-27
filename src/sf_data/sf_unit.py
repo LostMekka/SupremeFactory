d7d000013e852673b241f73f51e780b2773bb0de
LostMekka <lostmekka@web.de>
2015-06-27 16:53:52 +0200

    added unit update

--- src/sf_data/sf_unit.py
+++ src/sf_data/sf_unit.py
@@ -1,7 +1,11 @@
 class Unit:
 
-    pos = 0
-    speed = 0
-    hp = 10
-    attack = 1
-    range = 0
+    def __init__(self):
+        self.pos = 0
+        self.speed = 0
+        self.hp = 10
+        self.attack = 1
+        self.range = 0
+    
+    def update(self, time):
+        pass
