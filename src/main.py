63ec3cd1832747383ecb66244ad829d21a093ab4
Josephine Rehak <justallphine@googlemail.com>
2015-06-27 15:53:35 +0200

    added mouseX and mouseY position :P

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 import pygame, sys
 from pygame.locals import *
 from sf_tiles import sf_tile
+from utils import Colors 
 
 def draw_ui():
     for tile in sf_tile.tiles:
         pygame.draw.rect(DISPLAYSURF, tile.bg_color, tile.rect)
+		
+def drawMousePos():	
+    (mouseX, mouseY) = pygame.mouse.get_pos()
+    TextSurf = basicFont.render("X: "+str(mouseX)+" Y:" + str(mouseY), True, Colors.black)
+    TextRect= TextSurf.get_rect()
+    TextRect.center = ((mouseX+10),(mouseY))
+    DISPLAYSURF.blit(TextSurf, TextRect)		
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((1024, 700))
 pygame.display.set_caption('Hello World!')
-
-draw_ui()
+basicFont = pygame.font.SysFont(None, 48)
 
 while True:  # main game loop
+    draw_ui()
+    drawMousePos()
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             pygame.quit()
-            sys.exit()
+            sys.exit()	
     pygame.display.update()
-
+	
