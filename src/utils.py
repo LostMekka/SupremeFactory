8b33f01f896d5d522162d9b1c863f2386c97f3a2
Gilbert Röhrbein <gilbert@ifsr.de>
2015-06-27 13:18:03 +0200

    utils: Duct added

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+class Duct(dict):
+    '''
+    duck = Duct(voice = "quack", mass = 0.6)
+    assert duck.voice, "quack"
+    duck.spam = "ham"
+    '''
+
+    def __getattr__(self, k):
+        return self[k]
+
+    def __setattr__(self, k, v):
+        self[k] = v
